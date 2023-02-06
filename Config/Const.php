705311845600e88e91c65401c7448b4272e4fb16
alex2022cci <alex2022cci@outlook.fr>
2023-02-06 09:50:30 +0100

    1st commit

---
+++ Config/Const.php
@@ -0,0 +1,9 @@
+<?php
+
+define('__WEBROOT__', dirname(__FILE__));
+define('__ROOT__', dirname(__WEBROOT__));
+
+define('__DS__', DIRECTORY_SEPARATOR);
+define('__CONFIG__', __ROOT__ . __DS__ . 'Config');
+define('__BASE_URL__', dirname(dirname($_SERVER['SCRIPT_NAME'])));
+echo __BASE_URL__;
---
+++ public_html/index.php
@@ -0,0 +1,3 @@
+<?php
+
+include('../Config/Const.php');
